61d3b9ebcfb96cd48d8555c35e44367e557d5dee
NetworkingCode <79493127+NetworkingCode@users.noreply.github.com>
2024-07-03 23:49:18 +0000

    Instalado firebase con npm

--- login/firebase-init.js
+++ login/firebase-init.js
@@ -1,23 +1,21 @@
-// Importar las funciones que necesitas de los SDKs
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Tu configuración de Firebase
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCkDsufRf61kUxDFzC6TW8Q6WreYYObcYI",
-    authDomain: "promptest-f540a.firebaseapp.com",
-    projectId: "promptest-f540a",
-    storageBucket: "promptest-f540a.appspot.com",
-    messagingSenderId: "970757592350",
-    appId: "1:970757592350:web:c56e72227acfb3b34de50f",
-    measurementId: "G-M155PL62SH"
+  apiKey: "AIzaSyCkDsufRf61kUxDFzC6TW8Q6WreYYObcYI",
+  authDomain: "promptest-f540a.firebaseapp.com",
+  projectId: "promptest-f540a",
+  storageBucket: "promptest-f540a.appspot.com",
+  messagingSenderId: "970757592350",
+  appId: "1:970757592350:web:c56e72227acfb3b34de50f",
+  measurementId: "G-M155PL62SH"
 };
 
-// Inicializar Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
-
-// Exportar la instancia de autenticación
-export { auth };
