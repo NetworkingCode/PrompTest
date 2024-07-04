6e5d98997340f22b0795281a2ab9d05c01ed76bd
NetworkingCode <79493127+NetworkingCode@users.noreply.github.com>
2024-07-04 02:45:30 +0000

    Arreglando login

--- login/login.js
+++ login/login.js
@@ -36,6 +36,21 @@ loginForm.addEventListener('submit', (event) => {
           });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+     // Datos predeterminados para los campos del formulario
+     var datosPredeterminados = {
+       email: 'ricardonieto@live.cl',
+       password: '58931645'
+     };
+   
+     // Obtener el formulario de inicio de sesión
+     var loginForm = document.getElementById('login-form');
+   
+     // Asignar los valores predeterminados a los campos del formulario
+     loginForm.email.value = datosPredeterminados.email;
+     loginForm.password.value = datosPredeterminados.password;
+   });
+
 
 
 //Redirigir a la página principal después del inicio de sesión exitoso
