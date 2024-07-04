cdeaf3247d7b647f55de314ed5372f3d6fcc3f79
NetworkingCode <79493127+NetworkingCode@users.noreply.github.com>
2024-07-04 02:20:53 +0000

    Login solucionado, arreglando main.html

--- login/firebase-init.js
+++ login/firebase-init.js
@@ -1,4 +1,12 @@
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
